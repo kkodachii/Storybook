f060b4cac310f0ff0a48a67c469d1992ec7adcad
zekie1604 <keanlucas54926@gmail.com>
2025-11-16 22:01:27 +0800

    kn

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const defaultBook: Book = {
   illustrator: 'Pangaan ng Designer/Ilustrator',
   genre: 'Pantasya, Pang-edukasyon',
   isbn: '123-456750',
-  printCount: '1,000',
+  printCount: '1,00 0',
   date: 'Oktubre 26, 2004',
   synopsis: `Sa kaharian ng Kaalam, kilala si Prinsesa Usisa ng karamihan sa likas niyang pagiging matanong. Sa araw ng kanyang ika-sampung kaarawan, niregaluhan siya ng kanyang mga nakatatandang kapatid ng isang libro. Librong anila'y sasagot sa mga katanungan ng prinsesa. Lingid sa kanyang kaalaman, ang libro pala ay may taglay na hiwaga, dahilan upang siya ay mapadpad sa kakatwang mundo ng Lumina. Isang lugar na mayroong mahiwagang batis, kalito-litong panahon, buhay na mga larawan, at ginintuang mga dahon. Doon, si Prinsesa Usisa, kasama ang kanyang gabay na kwago ay naglakbay tungo sa pagtuklas ng pagbabago, pagpapabuti, at pagkilala sa sarili. 
 `,
